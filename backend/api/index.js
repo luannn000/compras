c927c9fda7ae10ce920a3576aef4f562a2605a16
Luan <69060192+luannn000@users.noreply.github.com>
2024-12-18 19:10:47 -0300

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,11 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(
-	cors({
-		origin: '*',
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		allowedHeaders: ['Content-Type'],
-	})
+	cors({})
 );
 
 app.get('/', (req, res) => res.json({ name: 'asas', date: 'aasas' }));
